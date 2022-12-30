7585472fb4ed2c1a58e3bf6131db011effdb66eb
ngocphuocha <phuoc04012000@gmail.com>
2022-12-30 19:40:17 +0700

    Add project files.

---
+++ FormularApi/Controllers/DriversController.cs
@@ -0,0 +1,66 @@
+﻿
+using FormularApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormularApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriversController : ControllerBase
+    {
+        private static List<Driver> _drivers = new List<Driver>()
+        {
+            new Driver()
+            {
+                Id = 1,
+                Name = "phuoc",
+                Team = "Mescedes",
+                DriverNumber = 44,
+            },
+            new Driver()
+            {
+                Id = 2,
+                Name = "Phuoc 2",
+                Team = "Lamboghini",
+                DriverNumber = 30
+            },
+             new Driver()
+             {
+                Id = 3,
+                Name = "Phuoc 3",
+                Team = "Ferarri",
+                DriverNumber = 99
+             }
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_drivers);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_drivers.FirstOrDefault(x => x.Id == id));
+        }
+
+        [HttpPost]
+        public IActionResult AddDriver(Driver driver)
+        {
+            _drivers.Add(driver);
+            return Ok(_drivers);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var driver = _drivers.FirstOrDefault(x => x.Id == id);
+
+            if (driver == null) return NotFound();
+
+            _drivers.Remove(driver);
+            return NoContent();
+        }
+    }
+}
---
+++ FormularApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormularApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
